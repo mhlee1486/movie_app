2e765c9d69e9d7768b355a27c3228f9ea53b470d
Sosimsimgo01 <mhlee1486@gmail.com>
2018-12-15 15:43:44 +0900

    Updates

---
+++ precache-manifest.fd1bf1401ff62ad0cdb19696778291ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "0b3c94a272540844a5f7",
+    "url": "/movie_app/static/js/main.0b3c94a2.chunk.js"
+  },
+  {
+    "revision": "7c8f41d282a7da190721",
+    "url": "/movie_app/static/js/1.7c8f41d2.chunk.js"
+  },
+  {
+    "revision": "0b3c94a272540844a5f7",
+    "url": "/movie_app/static/css/main.d57e6564.chunk.css"
+  },
+  {
+    "revision": "4a9b8d7774ad6f8eefcbde115ae4c85c",
+    "url": "/movie_app/index.html"
+  }
+];
